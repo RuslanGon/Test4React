489214a3a3fffc7f983cd43638fccdd83799027e
RuslanGon <ruslan441006@gmail.com>
2024-05-13 17:10:03 +0300

    wer

--- src/componetns/RefExample/RefExample.jsx
+++ src/componetns/RefExample/RefExample.jsx
@@ -4,17 +4,23 @@ import { useRef } from "react"
 const RefExample = () => {
 
 const buttonRef = useRef(null)
+const inpurRef = useRef(null)
 
 const handleClick = () => {
 console.log(getComputedStyle(buttonRef.current).height);
 console.log(getComputedStyle(buttonRef.current).width)
 }
 
+const inputClick = () => {
+console.log(inpurRef.current);
+}
 
   return (
     <div>
       <button ref={buttonRef} onClick={handleClick}>Click to do smth</button>
-      <input type="text" placeholder="enter smth" />
+      <br />
+      <button onClick={inputClick}>Click input</button>
+      <input ref={inpurRef} type="text" placeholder="enter smth" />
     </div>
   )
 }
