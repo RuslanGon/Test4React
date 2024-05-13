867f07a3df2692e9deca02f7f6c88a8ea3d0a6b7
RuslanGon <ruslan441006@gmail.com>
2024-05-13 16:04:57 +0300

    wer

--- src/AppHTTPRequest.jsx
+++ src/AppHTTPRequest.jsx
@@ -8,25 +8,6 @@ import ProductList from "./componetns/ProductList/ProductList";
 import SearchProducts from "./componetns/SearchProducts/SearchProducts";
 import { reguestProductsByQuery } from "./api/api";
 
-
-// [
-//     "id": 1,
-//     "title": "iPhone 9",
-//     "description": "An apple mobile which is nothing like apple",
-//     "price": 549,
-//     "discountPercentage": 12.96,
-//     "rating": 4.69,
-//     "stock": 94,
-//     "brand": "Apple",
-//     "category": "smartphones",
-//     "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-//     "images": [
-//       "https://cdn.dummyjson.com/product-images/1/1.jpg",
-//       "https://cdn.dummyjson.com/product-images/1/2.jpg",
-//       "https://cdn.dummyjson.com/product-images/1/3.jpg",
-//       "https://cdn.dummyjson.com/product-images/1/4.jpg",
-//       "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
-//     ]
 const AppHTTPRequest = () => {
 
 const [products, setProducts] = useState(null)
